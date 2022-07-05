e3f5957885100cbe7363f121240e5bd940ca2db0
rogelcristobal <rogel.cristobal11@gmail.com>
2022-07-05 13:28:27 +0800

    react-redux-toolkit

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'   
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/counterSlice'   
 const store = configureStore({
   reducer:{
-    
+    counter: counterReducer
   } 
 })
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div className='h-screen border-sample	 w-80'>
+      
+    </div>
+  );
+}
+
+export default Sidebar;
