8c2b37ddd790c42b23afea42b91c7b12d1a2964e
rogel cristobal <rogel.cristobal11@gmail.com>
2022-07-08 19:47:21 +0800

    category page display 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,91 +2,187 @@ import { useState, useContext } from "react";
 import MovieContext from "./context/MovieContext";
 import CardComponent from "./components/CardComponent";
 import Grid from "@mui/material/Grid";
-import { Paper, Typography } from "@mui/material";
+import { Paper, Typography, Box, Link } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 function App() {
-  const { upcomingMovies, genre } = useContext(MovieContext);
-  const theme = createTheme({
-    typography: {
-      fontFamily: ["outfit", "Supreme", "Clash Display", "sans-serif"].join(
-        ","
-      ),
-    },
-  });
-  // styles
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="font-outfit relative flex w-full   h-screen bg-color-100 ">
-        {/* nav */}
-        <div className="fixed top-0 left-0 w-full h-16 z-20 bg-color-100 shadow-sm">
-          <div className="container h-full flex  px-4 justify-end mx-auto  rounded-lg">
-            {/* nav contents here */}
-            <Paper
-              variant="contained"
-              className="h-full w-full bg-inherit"
-              
-            ></Paper>
-          </div>
-        </div>
+	const { upcomingMovies, genre } = useContext(MovieContext);
+	const theme = createTheme({
+		typography: {
+			fontFamily: ["outfit", "Supreme", "Clash Display", "sans-serif"].join(
+				","
+			),
+		},
+	});
+	const itemThree = upcomingMovies?.results.slice(0, 3);
+	const randomMovie = Math.floor(
+		Math.random() * upcomingMovies?.results.length
+	);
+	console.log(randomMovie);
 
-        {/* sidebar */}
-        <div className=" w-96  rounded-lg h-full z-50 ">
-          {/* sidebar contents here */}
-          <Paper
-            variant="contained"
-            className="h-full w-full bg-inherit"
-            square
-          ></Paper>
-        </div>
+	// styles
+	return (
+		<ThemeProvider theme={theme}>
+			<div className="font-outfit relative flex w-full   h-screen bg-color-100 ">
+				{/* nav */}
+				<div className="fixed top-0 left-0 w-full h-16 z-20 bg-color-100 shadow-sm">
+					<div className="container h-full flex  px-4 justify-end mx-auto  rounded-lg">
+						{/* nav contents here */}
+						<Paper
+							variant="contained"
+							className="h-full w-full bg-inherit"
+						></Paper>
+					</div>
+				</div>
 
-        {/* body */}
-        <div className=" px-10  h-screen overflow-y-auto  no-scrollbar w-full ">
-          <div className="h-auto mt-24 p-4   mb-16 w-full   rounded-lg">
-            {/* contents here */}
-            <Paper
-              variant="contained"
-              className="h-screen flex flex-col items-start space-y-4 justify-start   pb-8 "
-              component="div"
-              
-            >
-              <Typography
-                variant="h6"
-                className="font-semibold text-3xl text-color-black pb-4"
-              >
-                Upcoming
-              </Typography>
+				{/* sidebar */}
+				<div className=" w-96  rounded-lg h-full z-50 ">
+					{/* sidebar contents here */}
+					<Paper
+						variant="contained"
+						className="h-full w-full bg-inherit"
+						square
+					></Paper>
+				</div>
 
-              <Paper
-                component="div"
-                variant="outlined"
-                className=" h-full w-full bg-inherit"
-              >
+				{/* body */}
+				<div className=" px-10  h-screen overflow-y-auto  no-scrollbar w-full ">
+					<div className="h-auto mt-24 p-4   mb-16 w-full   rounded-lg">
+						{/* contents here */}
+						<Paper
+							variant="contained"
+							className="min-h-screen h-auto flex flex-col items-start space-y-8 justify-start   pb-8 "
+							component="div"
+						>
+							<div className="flex flex-col items-center justify-center w-full  py-2 px-2">
+								<Box className="flex items-center justify-between w-full  mb-4">
+									<Typography
+										variant="h6"
+										className="font-semibold text-3xl text-color-black "
+									>
+										Upcoming
+									</Typography>
+									<Link
+										underline="none"
+										component="button"
+										variant="h6"
+										className="text-lg text-color-400 capitalize hover:text-color-500"
+										onClick={() => {
+											console.info("I'm a button.");
+										}}
+									>
+										see all
+									</Link>
+								</Box>
 
-                {/* <Grid container spacing={2} rowSpacing={6} columns={4}>
-                  {upcomingMovies?.results.map((item, id) => (
-                    <Grid item xs={1} key={id}>
-                      <CardComponent data={item} key={id} />
-                    </Grid>
-                  ))}
-                </Grid> */}
-              </Paper>
-            </Paper>
-          </div>
-        </div>
+								<Paper
+									component="div"
+									variant="contained"
+									className=" h-full w-full bg-inherit rounded-lg"
+								>
+									{/* map only 3 item */}
+									<Grid container spacing={2} columns={3}>
+										{itemThree.map((item, id) => (
+											<Grid item xs={1} key={id}>
+												<CardComponent data={item} />
+											</Grid>
+										))}
+									</Grid>
+									{/* map whole page item */}
+									{/* <Grid container spacing={2} rowSpacing={6} columns={3}>
+									{upcomingMovies?.results.map((item, id) => (
+										<Grid item xs={1} key={id}>
+										<CardComponent data={item}  />
+										</Grid>
+									))}
+								</Grid> */}
+								</Paper>
+							</div>
+							{/* category 2 */}
+							<div className="flex flex-col items-center justify-center w-full  py-2 px-2">
+								<Box className="flex items-center justify-between w-full  mb-4">
+									<Typography
+										variant="h6"
+										className="font-semibold text-3xl text-color-black "
+									>
+										Popular Now
+									</Typography>
+									<Link
+										underline="none"
+										component="button"
+										variant="h6"
+										className="text-lg text-color-400 capitalize hover:text-color-500"
+										onClick={() => {
+											console.info("I'm a button.");
+										}}
+									>
+										see all
+									</Link>
+								</Box>
+								<Paper
+									component="div"
+									variant="contained"
+									className=" h-full w-full bg-inherit rounded-lg"
+								>
+									<Grid container spacing={2} columns={3}>
+										{itemThree.map((item, id) => (
+											<Grid item xs={1} key={id}>
+												<CardComponent data={item} />
+											</Grid>
+										))}
+									</Grid>
+								</Paper>
+							</div>
+							{/* category 3 */}
+							<div className="flex flex-col items-center justify-center w-full  py-2 px-2">
+								<Box className="flex items-center justify-between w-full  mb-4">
+									<Typography
+										variant="h6"
+										className="font-semibold text-3xl text-color-black "
+									>
+										Top Rated
+									</Typography>
+									<Link
+										underline="none"
+										component="button"
+										variant="h6"
+										className="text-lg text-color-400 capitalize hover:text-color-500"
+										onClick={() => {
+											console.info("I'm a button.");
+										}}
+									>
+										see all
+									</Link>
+								</Box>
+								<Paper
+									component="div"
+									variant="contained"
+									className=" h-full w-full bg-inherit rounded-lg"
+								>
+									<Grid container spacing={2} columns={3}>
+										{itemThree.map((item, id) => (
+											<Grid item xs={1} key={id}>
+												<CardComponent data={item} />
+											</Grid>
+										))}
+									</Grid>
+								</Paper>
+							</div>
+						</Paper>
+					</div>
+				</div>
 
-        {/*page nav*/}
-        <div className="w-72 mt-24 ml-0 mr-8  rounded-lg h-96">
-          {/* page nav contents here */}
-          <Paper
-            variant="contained"
-            className="h-full w-full bg-inherit"
-            
-          ></Paper>
-        </div>
-      </div>
-    </ThemeProvider>
-  );
+				{/*page nav*/}
+				<div className="w-72 mt-24 ml-0 mr-8  rounded-lg h-96">
+					{/* page nav contents here */}
+					<Paper
+						variant="contained"
+						className="h-full w-full bg-inherit"
+					></Paper>
+				</div>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import { Card, CardMedia,CardContent, Typography} from "@mui/material";
-const CardComponent = ({data}) => {
-  return (
-    <Card variant="outlined"   className="cursor-pointer w-full  bg-color-200 rounded-lg" onClick={()=>alert(`${data.id}\n${data.title}`)}>
-      <div className='h-32 w-full bg-color-300'></div>
-    {/* <CardMedia  component='img'
+import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+const CardComponent = ({ data }) => {
+	return (
+		<Card
+			variant="contained"
+			className="cursor-pointer w-full  bg-color-100 rounded-lg"
+			onClick={() => alert(`${data.id}\n${data.title}`)}
+		>
+			<div className="h-32 w-full bg-color-300 rounded-lg"></div>
+			{/* <CardMedia  component='img'
     className="h-32 w-full object-cover" title="" 
     image={`https://image.tmdb.org/t/p/w500/${data.poster_path}`} /> */}
-    <CardContent className="flex flex-col px-4">
-      <Typography variant="p" className="font-bold text-sm tracking-wide  text-color-black" color="initial">
-        {data.title}
-      </Typography>
-      
-    </CardContent>
-    
-
-  </Card>
-
-  );
-}
-
+			<CardContent className="flex flex-col px-4">
+				<Typography
+					variant="p"
+					className="font-semibold text-sm tracking-wide  text-color-500"
+					color="initial"
+				>
+					{data.title}
+				</Typography>
+			</CardContent>
+		</Card>
+	);
+};
 
 export default CardComponent;
