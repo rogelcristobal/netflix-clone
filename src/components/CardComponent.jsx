616eb6bd303cac7627e95c27c8b06dcef28fe4d6
rogelcristobal <rogel.cristobal11@gmail.com>
2022-07-11 13:07:47 +0800

    redesign cards and skeleton

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,55 +1,118 @@
 import React from "react";
 import { motion } from "framer-motion";
 import {
-	Card,
-	CardMedia,
-	CardContent,
-	Typography,
-	Skeleton,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Skeleton,
 } from "@mui/material";
-const CardComponent = ({ data }) => {
-	return (
-		<motion.div whileHover={{ scale: 1.07 }} whileTap={{ scale: 1 }}>
-			<Card
-				variant="contained"
-				className="cursor-pointer w-full  bg-color-100 rounded-lg"
-				// onClick={() => alert(`${data.id}\n${data.title}`)}
-			>
-				{data ? (
-					<CardMedia
-						component="img"
-						className="h-32 w-full object-cover"
-						title=""
-						image={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
-					/>
-				) : (
-					<Skeleton
-						variant="rectangular"
-						animation="wave"
-						className="h-32 w-full"
-					></Skeleton>
-				)}
 
-				<CardContent className="flex flex-col px-4">
-					{data ? (
-						<Typography
-							variant="p"
-							className="font-semibold text-sm tracking-wide  text-color-500"
-							color="initial"
-						>
-							{data.title}
-						</Typography>
-					) : (
-						<Skeleton
-							variant="rectangular"
-							animation="wave"
-							className="h-5 w-full"
-						></Skeleton>
-					)}
-				</CardContent>
-			</Card>
-		</motion.div>
-	);
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const CardComponent = ({ data }) => {
+  console.log(data);
+  return (
+    <motion.div whileHover={{ scale: 1.07 }} whileTap={{ scale: 1 }}>
+      <Card
+        variant="contained"
+        className="cursor-pointer w-36 h-fit pb-2  bg-color-100 rounded-lg"
+        // onClick={() => alert(`${data.id}\n${data.title}`)}
+      >
+        {!data ? (
+          <CardMedia
+            component="img"
+            className="h-48 w-full object-cover rounded-lg"
+            title=""
+            image={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+          />
+        ) : (
+          <Skeleton
+            variant="rectangular"
+            animation="wave"
+            className="h-48 rounded-lg w-full"
+          ></Skeleton>
+        )}
+
+        <CardContent className="flex flex-col relative p-2 ">
+          <div className="h-9 w-9 absolute -top-6 right-4">
+            <CircularProgressbar
+              value={`${!data ? data.vote_average:0}`}
+              maxValue={10}
+              text={`${!data ? data.vote_average: 0}`}
+              styles={buildStyles({
+                // Rotation of path and trail, in number of turns (0-1)
+                rotation: 0,
+
+                // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                strokeLinecap: "round",
+
+                // Text size
+                textSize: "2rem",
+
+                // How long animation takes to go from one percentage to another, in seconds
+                pathTransitionDuration: 0.5,
+
+                // Can specify path transition in more detail, or remove it entirely
+                // pathTransition: 'none',
+
+                // Colors
+				pathColor: `rgba(62, 152, 199)`,
+				textColor: '#f88',
+				trailColor: '#d6d6d6',
+				backgroundColor: '#3e98c7',
+              })}
+              strokeWidth="12"
+              className=" p-1 rounded-full bg-white"
+            />
+          </div>
+
+          {!data ? (
+			<>
+            <Typography
+              variant="p"
+              className="font-medium  text-left tracking-wide mt-4 w-fit  text-black"
+              color="initial"
+			  >
+              {data?.title}
+            </Typography>
+			<Typography
+			variant="p"
+			className="font-medium w-fit h-fit mt-2 text-xs text-left tracking-wide  text-color-500"
+			color="initial"
+			>
+			{data?.release_date}
+		  </Typography>
+			</>
+          ) : (
+			<>
+            <Skeleton
+              animation="wave"
+			  >
+			<Typography
+              variant="p"
+              className="font-medium text-left tracking-wide mt-4   text-black"
+              color="initial"
+			  >
+              {data?.title}
+            </Typography>
+			</Skeleton>
+			<Skeleton>
+			<Typography
+			variant="p"
+			className="font-medium w-fit h-fit mt-2 text-xs text-left tracking-wide  text-color-500"
+			color="initial"
+			>
+			{data?.release_date}
+		  </Typography>
+			</Skeleton>
+			</>
+          )}
+        </CardContent>
+      </Card>
+    </motion.div>
+  );
 };
 
 export default CardComponent;
---
+++ src/components/TripleCardComponent.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Box, Typography, Link, Paper, Grid } from "@mui/material";
+import CardComponent from "./CardComponent";
+const TripleCardComponent = ({ upcomingMovies ,title}) => {
+//   gets first 3 items in the returned array
+const fetchThreeItem = (endpoint) => {
+    return endpoint?.results.slice(0, 5);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full  py-2 ">
+        {/* title and CTA */}
+      <Box className="flex items-center justify-between w-full  mb-4">
+        <Typography
+          variant="h6"
+          className="font-semibold capitalize text-2xl text-color-black "
+        >
+          {title}
+        </Typography>
+        <Link
+          underline="none"
+          component="button"
+          variant="p"
+          className="text-sm text-color-400 font-medium font-outfit capitalize hover:text-color-500"
+          onClick={() => {
+            alert("I'm a button.");
+          }}
+        >
+          see all
+        </Link>
+      </Box>
+
+          {/* used paper as a container */}
+      <Paper
+        component="div"
+        variant="contained"
+        className=" h-full w-fit  py-6 px-6 grid place-content-center rounded-lg "
+      >
+        {/* map only 3 item */}
+        <Grid container spacing={2} columns={5}>
+          {(!upcomingMovies
+            ? Array.from(new Array(5))
+            : fetchThreeItem(upcomingMovies)
+          ).map((item, id) => (
+            <Grid item xs={1} key={id}>
+              <CardComponent data={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Paper>
+    </div>
+  );
+};
+
+export default TripleCardComponent;
