d6e43b2030e2e6c765c4012120c974659406714b
rogelcristobal <rogel.cristobal11@gmail.com>
2022-08-02 08:09:29 +0800

    redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route, NavLink } from "react-router-dom";
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { FiUser, FiSettings, FiMenu } from "react-icons/fi";
+import { FiUser, FiSettings } from "react-icons/fi";
+import {HiMenuAlt4} from "react-icons/hi"
 // pages
 import MovieCategories from "./pages/MovieCategories";
 import TVCategories from "./pages/TVCategories";
@@ -24,7 +25,6 @@ const queryClient = new QueryClient();
 
 function App() {
   const nav = useNavigate();
-  const [menuStatus, setMenuStatus] = useState(true);
   const theme = createTheme({
     typography: {
       fontFamily: ["outfit", "Supreme", "Clash Display", "sans-serif"].join(
@@ -59,46 +59,21 @@ function App() {
           <Route
             path="/homepage/*"
             element={
-              <div className="font-outfit relative flex w-full   h-screen bg-[#161c24] dark:bg-[#1b2635] ">
+              <div className="font-outfit relative flex w-full   h-screen bg-color-100 dark:bg-[#1b2635]   ">
                 {/* nav */}
-                <div className="fixed top-0 left-0 w-full h-16 z-10  ">
-                  <div className="container h-full flex pt-4 px-4 justify-end mx-auto items-center rounded-lg ">
+                <div className="fixed top-0 left-0 w-full h-20 z-20 ">
+                  <div className="container h-full flex  justify-end mx-auto items-center rounded-lg ">
                     {/* nav contents here */}
                     <Paper
-                      variant="contained"
-                      className={`h-full ${
-                        !menuStatus ? "w-full" : "w-[calc(100%-17rem)]"
-                      } bg-inherit flex items-center justify-between px-4 transition-all ease-in-out duration-500`}
+                      variant="outlined"
+                      className={`h-full w-full  bg-inherit flex items-center justify-between  transition-all ease-in-out duration-500`}
                     >
                       {/* ham */}
-                      <IconButton
-                        onClick={() => setMenuStatus(!menuStatus)}
-                        aria-label="log-in"
-                        size="small"
-                        className={`text-color-100 rounded-lg  ${
-                          !menuStatus
-                            ? "bg-primary-400 hover:bg-primary-300"
-                            : "bg-inherit hover:bg-primary-400/25"
-                        } space-x-4 p-2   hover:text-color-100 transition duration-500 ease-in-out`}
-                      >
-                        <FiMenu></FiMenu>
-                      </IconButton>
+                    
 
-                      <div className="flex items-center justify-between space-x-4">
-                        <IconButton
-                          aria-label="log-in"
-                          size="small"
-                          className="text-color-100 rounded-lg   space-x-4 p-2  hover:bg-primary-400 hover:text-color-100 transition duration-500 ease-in-out"
-                        >
-                          <FiSettings></FiSettings>
-                        </IconButton>
-                        <IconButton
-                          aria-label="log-in"
-                          size="small"
-                          className="text-color-100 rounded-lg    p-2  hover:bg-primary-400 hover:text-color-100 transition-all duration-500 ease-in-out"
-                        >
-                          <FiUser></FiUser>
-                        </IconButton>
+                      <div className="flex items-center justify-between w-full space-x-4">
+                     
+                    
                       </div>
                     </Paper>
                   </div>
@@ -106,70 +81,20 @@ function App() {
 
                 {/* sidebar */}
                 <div
-                  className={` ${
-                    !menuStatus ? "w-2" : "w-80"
-                  } z-10  h-full  overflow-x-hidden transition-all ease-in-out duration-500`}
+                  className={` w-80 z-10  h-full  overflow-x-hidden transition-all ease-in-out duration-500`}
                 >
                   {/* sidebar contents here */}
                   <Paper
-                    variant="contained"
-                    className=" w-full   h-full pt-20 box-border bg-[#19212b] dark:bg-[#233044] flex flex-col items-center justify-start "
+                    variant="outlined"
+                    className=" w-full px-4  h-full pt-20 box-border bg-color-100 dark:bg-[#19212b]  flex flex-col items-center justify-start "
                     square
                   >
-                    <Paper
-                      variant="contained"
-                      sx={{ overflow: "auto", boxSizing: "border-box" }}
-                      className="  h-fit py-2 w-56 rounded-lg  bg-inherit"
-                    >
-                      <List>
-                        {[
-                          {
-                            path: "/",
-                            title: "movies",
-                          },
-                          {
-                            path: "/tvShows",
-                            title: "tv shows",
-                          },
-                        ].map((item, id) => (
-                          <NavLink
-                            to={`/homepage${item.path}`}
-                            key={id}
-                            className="no-underline "
-                          >
-                            {({ isActive }) => (
-                              <>
-                                <ListItem disablePadding>
-                                  <ListItemButton
-                                    className={` rounded-lg  px-6  py-3 my-1 ${
-                                      isActive
-                                        ? " bg-primary-400/10"
-                                        : "bg-inherit"
-                                    }`}
-                                  >
-                                    <Typography
-                                      variant="p"
-                                      className={`capitalize  font-[400] tracking-wide  text-sm ml-4 ${
-                                        isActive
-                                          ? "text-color-100"
-                                          : "text-color-500"
-                                      }	`}
-                                    >
-                                      {item.title}
-                                    </Typography>
-                                  </ListItemButton>
-                                </ListItem>
-                              </>
-                            )}
-                          </NavLink>
-                        ))}
-                      </List>
-                    </Paper>
+                   
                   </Paper>
                 </div>
                 {/* body */}
 
-                <div className="flex items-start justify-start w-full pt-20 box-border">
+                <div className="flex items-start justify-start w-full pt-16 box-border">
                   <Routes>
                     <Route path="/" element={<MovieCategories />}></Route>
                     <Route path="/result" element={<SearchResults />}></Route>
@@ -193,3 +118,7 @@ export default App;
 // https://dribbble.com/shots/6839523-Music-App
 // https://berrydashboard.io/free/
 // https://demos.wrappixel.com/premium-admin-templates/react/flexy-react/dark/#/dashboards/dashboard1
+
+// netflix
+
+// https://dribbble.com/shots/14655584-White-and-clean-TV-app
--- src/pages/MovieCategories.jsx
+++ src/pages/MovieCategories.jsx
@@ -11,13 +11,11 @@ const MovieCategories = () => {
   return (
     <>
       <div className=" px-4 pt-2 box-border flex flex-col items-center justify-start  h-full overflow-y-auto space-y-5  w-full ">
-        <div className="h-auto  min-h-full   box-border flex flex-col items-start  justify-start    bg-[#151920] gap-8   w-full   rounded-2xl">
+        <div className="h-auto  min-h-full   box-border flex flex-col items-start  justify-start    gap-8   w-full   rounded-2xl">
           <div className="px-4 space-y-5 py-4 h-full w-full box-border ">
             <Grid container spacing={2} columns={4} className="h-full">
               <Grid item xs={2}>
-                <div className="w-full h-full bg-[#161c24] bg-opacity-1 backdrop-blur-lg rounded-2xl text-white text-center">
-                  a
-                </div>
+                
               </Grid>
             </Grid>
           </div>
