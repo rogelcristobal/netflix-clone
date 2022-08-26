13027a3329951c9ddfb41123c36da66623c3ecc0
rogelcristobal <rogel.cristobal11@gmail.com>
2022-08-26 14:22:13 +0800

    weeekly save

--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -1,6 +1,6 @@
-import { Paper, Stack ,Box} from "@mui/material";
+import { Paper, Stack ,Box, Button} from "@mui/material";
 import { useParams, useLocation } from "react-router-dom";
-import useFetchGenreByCategory from "../fetch/general/useFetchGenreByCategory";
+import {MdPlayArrow} from 'react-icons/md'
 import useFetchItem from "../fetch/general/useFetchItem";
 import GenreChip from "../components/GenreChip";
 const ItemPage = () => {
@@ -19,7 +19,7 @@ const ItemPage = () => {
       >
         {/* scrollable content */}
         <div className="overflow-y-scroll overflow-x-hidden h-full w-full px-0   pb-72 box-border space-y-2 scroll-smooth	">
-          <div className="lg:h-[80vh]  w-full relative box-border  overflow-hidden">
+          <div className="lg:h-[85vh]  w-full relative box-border  overflow-hidden">
             <img
               className="h-full  w-full object-cover "
               src={`https://image.tmdb.org/t/p/w1280${data?.backdrop_path}`}
@@ -28,9 +28,9 @@ const ItemPage = () => {
             <div className="h-full w-full absolute  top-0 left-0  backdrop-brightness-40	bg-gradient-to-r from-[#0d1117]"></div>
             <div className="h-full w-full absolute  top-0 left-0  backdrop-brightness-40	bg-gradient-to-t from-[#0d1117]"></div>
 
-            <Box className=" absolute top-60 left-16 box-border">
+            <Box className=" absolute top-52 left-16 box-border">
               {/* titles */}
-              <h5 className=" text-white text-5xl   font-bold box-border  m-0 mb-6">
+              <h5 className=" text-white text-5xl   font-bold box-border  m-0 mb-16">
                 {data?.title}
                 {data?.name}
               </h5>
@@ -43,14 +43,20 @@ const ItemPage = () => {
                         itemID={item}
                         path={pathname}
                         key={id}
-                        sx="bg-gray-600/30 hover:bg-gray-400/30 py-1"
+                        sx="bg-gray-700/30 hover:bg-gray-600/30 py-1 "
                       ></GenreChip>
                     ))
                   : null}
               </Stack>
 
               {/* CTAs */}
-              <Box>
+              <Box className="w-96 mt-6 flex justify-start space-x-6">
+                <Button variant="contained" className=" rounded-full px-12 text-sm py-2 capitalize shadow-none  bg-primary-400 drop-shadow-none" startIcon={<MdPlayArrow></MdPlayArrow>} >
+                  Watch 
+                </Button>
+                <Button variant="contained" className=" rounded-full px-12 text-sm py-2 capitalize shadow-none  bg-transparent hover:bg-gray-600/30 outline-1  outline-color-300 outline drop-shadow-none" startIcon={<MdPlayArrow></MdPlayArrow>} >
+                   trailer 
+                </Button>
 
               </Box>
               {/* storyline */}
